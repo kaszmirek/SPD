7765b5c4bb7e407d2da0de14ac18daa3f1a1626c
Kaszmirek <krisor123@gmail.com>
2021-04-01 17:09:51 +0200

    zad 1 wersja do oddania

--- SPD_1/SPD_zad1.py
+++ SPD_1/SPD_zad1.py
@@ -1,5 +1,7 @@
 import Schrage
+import Carlier
 from RandomNumberGenerator import RandomNumberGenerator as Rng
+
 
 seed = int(input("Ziarno: "))
 n = int(input("Ilosc elementow: "))
@@ -34,7 +36,7 @@ for i in range(n):
     Q_j.append(q_j)
 
 S.append(R_j[0])
-C.append(P_j[0])
+C.append(S[0] + P_j[0])
 Cq.append(C[0] + Q_j[0])
 C_max = C[0] + Q_j[0]
 
@@ -42,7 +44,7 @@ for i in range(1, n):
     S.append(max(R_j[i], C[i-1]))
     C.append(S[i] + P_j[i])
     Cq.append(C[i] + Q_j[i])
-    C_max = max(C_max, Cq[i-1])
+    C_max = max(C_max, Cq[i])
 
 
 for i in range(n):
@@ -52,6 +54,10 @@ print(f'nr:{J}')
 print(f'r:{R_j}')
 print(f'p:{P_j}')
 print(f'q:{Q_j}')
+print()
+print(f'S: {S}')
+print(f'C: {C}')
+print(f'Cq: {Cq}')
 print()
 
 for i in range(n):
@@ -67,7 +73,7 @@ PI.clear()
 Schrage.schrage(J, R_j, P_j, Q_j, PI)
 
 S.append(R_j[PI[0]])
-C.append(P_j[PI[0]])
+C.append(S[0] + P_j[PI[0]])
 Cq.append(C[0] + Q_j[PI[0]])
 C_max = C[0] + Q_j[PI[0]]
 
@@ -75,10 +81,10 @@ for i in range(1, n):
     S.append(max(R_j[PI[i]], C[i-1]))
     C.append(S[i] + P_j[PI[i]])
     Cq.append(C[i] + Q_j[PI[i]])
-    C_max = max(C_max, Cq[i-1])
+    C_max = max(C_max, Cq[i])
 
 
-print('Po zastosowaniu algorytmu:')
+print('Po zastosowaniu algorytmu bez przerwan:')
 for i in range(n):
     PI[i] += 1
 
@@ -103,7 +109,7 @@ PI.clear()
 Schrage.schrage_k(J, R_j, P_j, Q_j, PI)
 
 S.append(R_j[PI[0]])
-C.append(P_j[PI[0]])
+C.append(S[0] + P_j[PI[0]])
 Cq.append(C[0] + Q_j[PI[0]])
 C_max = C[0] + Q_j[PI[0]]
 
@@ -111,7 +117,7 @@ for i in range(1, n):
     S.append(max(R_j[PI[i]], C[i-1]))
     C.append(S[i] + P_j[PI[i]])
     Cq.append(C[i] + Q_j[PI[i]])
-    C_max = max(C_max, Cq[i-1])
+    C_max = max(C_max, Cq[i])
 
 
 print('Po zastosowaniu algorytmu z kolejka:')
@@ -135,7 +141,7 @@ PI.clear()
 
 # ____________________ Algorytm z przerwaniami _______________________
 
-C_max = Schrage.schrage_pmtn(J, R_j, P_j, Q_j, PI)
+C_max = Schrage.schrage_pmtn(J, R_j.copy(), P_j.copy(), Q_j.copy(), PI)
 
 print('Po zastosowaniu algorytmu z przerwaniami:')
 for i in range(len(PI)):
@@ -148,29 +154,52 @@ print()
 for i in range(len(PI)):
     PI[i] -= 1
 
+C_max = 0
 S.clear()
 C.clear()
 Cq.clear()
 PI.clear()
 
+
 # ____________________ Carlier _______________________
 
 UB = 999999999999
+print(R_j)
+print(P_j)
+print()
+Carlier.carlier(UB, J, R_j, P_j, Q_j, PI, S, C, Cq, n)
 
-Schrage.schrage(J, R_j, P_j, Q_j, PI)
+print(f'p: {P_j}')
+print(f'r: {R_j}')
+print(f'q: {Q_j}')
+
+C_max = 0
+S.clear()
+C.clear()
+Cq.clear()
 
 S.append(R_j[PI[0]])
-C.append(P_j[PI[0]])
+C.append(S[0] + P_j[PI[0]])
 Cq.append(C[0] + Q_j[PI[0]])
 C_max = C[0] + Q_j[PI[0]]
 
-for i in range(1, n):
+for i in range(1, len(PI)):
     S.append(max(R_j[PI[i]], C[i-1]))
     C.append(S[i] + P_j[PI[i]])
     Cq.append(C[i] + Q_j[PI[i]])
-    C_max = max(C_max, Cq[i-1])
+    C_max = max(C_max, Cq[i])
 
-U = C_max
-if U < UB:
-    UB = U
-    PI_p = PI
+
+print('Algorytm Carliera: ')
+for i in range(len(PI)):
+    PI[i] += 1
+
+print(f'PI:{PI}')
+print(f'S:{S}')
+print(f'C:{C}')
+print(f'Cq:{Cq}')
+print(f'Cmax: {C_max}')
+print()
+
+for i in range(len(PI)):
+    PI[i] -= 1
